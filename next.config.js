1ff520c5ceeceaee60fa039bc5c9fa9d2c5fafd9
DESKTOP-4AGI8G8\donke <sd789852t@gmail.com>
2022-01-20 00:25:07 +0800

    test scss

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
-const path = require("path");
-
 module.exports = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   reactStrictMode: true,
-  sassOptions: {
-    modules: {
-      localIdentName: "[hash:base64:5]",
-    },
-    includePaths: [path.join(__dirname, "styles")],
-  },
 };

029a94f3598f5ea83eb8e7fb7901310c68040f44
Frenkieli <49364993+Frenkieli@users.noreply.github.com>
2022-01-21 17:17:50 +0800

    feat: nextjs  lesson end

---
+++ pages/_document.js
@@ -0,0 +1,30 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+
+
+// 這些是必要的屬性
+// Html, Head, Main, NextScript
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html
+        lang="zh-tw"
+        className="bg-orange-300"
+      >
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;

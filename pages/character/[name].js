44da47e791e732597058d8a62e622c110d80b75c
Frenkieli <49364993+Frenkieli@users.noreply.github.com>
2022-01-24 11:11:46 +0800

    feat: 撰寫 meta 和整理資料

--- pages/character/[name].js
+++ pages/character/[name].js
@@ -1,4 +1,4 @@
-// import Head from "next/head";
+import Head from "next/head";
 // import Layout from "../../components/layout";
 // import Date from "../../components/date";
 // import { getAllPostIds, getPostData } from "../../lib/posts";
@@ -6,16 +6,28 @@
 // import utilStyles from "../../styles/utils.module.scss";
 import getCharactersData from "../../lib/getdata";
 
-export default function Post({ postData }) {
+export default function Post({ characterData }) {
   return (
     <>
-      {JSON.stringify(postData)}
+      <Head>
+        <link rel="shortcut icon" href={ '/images/' + characterData.class + '/icon.png' }/>
+        <meta name="description" content={characterData.description}/>
+        <meta name="author" content="驢子"/>
+        <meta name="copyright" content="驢形工作室"/>
+        <title>{characterData.name + '的冒險紀錄'}</title>
+      </Head>
+      {JSON.stringify(characterData)}
     </>
   );
 }
 
 export async function getStaticPaths() {
   const paths = await getCharactersData();
+  for(let i = 0 ; i < paths.length ; i++) {
+    paths[i] = {
+      params: paths[i]
+    }
+  }
   return {
     paths,
     fallback: false,
@@ -23,10 +35,17 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = await getCharactersData();
+  const data = await getCharactersData();
+  let characterData;
+  for(let i = 0 ; i < data.length ; i++) {
+    if(data[i].name === params.name) {
+      characterData = data[i];
+      break;
+    }
+  }
   return {
     props: {
-      postData,
+      characterData: characterData,
     },
   };
 }

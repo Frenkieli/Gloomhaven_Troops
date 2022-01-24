0a8e57d196cc69be17b95146cae79fce898cea61
Frenkieli <49364993+Frenkieli@users.noreply.github.com>
2022-01-24 12:32:07 +0800

    feat: prefix 新增

--- pages/character/[name].js
+++ pages/character/[name].js
@@ -13,18 +13,18 @@ export default function Post({ characterData }) {
         {/* header icon */}
         <link rel="shortcut icon" href={ '/images/' + characterData.class + '/icon.png' }/>
         {/* 手機用 icon -precomposed 意思是說不要反光*/}
-        <link rel="apple-touch-icon-precomposed" sizes="192x192" href={ '/images/' + characterData.class + '/icon.png' }/>
+        <link rel="apple-touch-icon-precomposed" sizes="192x192" href={ 'https://frenkieli.github.io/Gloomhaven_Troops/images/' + characterData.class + '/icon.png' }/>
         <title>{characterData.name + '的冒險紀錄'}</title>
         <meta name="author" content="驢子"/>
         <meta name="copyright" content="驢形工作室"/>
         <meta name="name" content={characterData.name + '的冒險紀錄'}/>
-        <meta name="image" content={ '/images/' + characterData.class + '/icon.png' }/>
+        <meta name="image" content={ 'https://frenkieli.github.io/Gloomhaven_Troops/images/' + characterData.class + '/icon.png' }/>
         <meta name="description" content={characterData.description}/>
         <meta property="og:title" content={characterData.name + '的冒險紀錄'}/>
         <meta property="og:type" content="article"/>
         <meta property="article:section" content={characterData.name}/>
         <meta property="og:url" content={"https://frenkieli.github.io/Gloomhaven_Troops/character/" + characterData.name}/>
-        <meta property="og:image" content={ '/images/' + characterData.class + '/icon.png' }/>
+        <meta property="og:image" content={ 'https://frenkieli.github.io/Gloomhaven_Troops/images/' + characterData.class + '/icon.png' }/>
         <meta property="og:description" content={characterData.description} />
         <meta property="og:site_name" content="幽港迷城冒險紀錄" />
       </Head>
